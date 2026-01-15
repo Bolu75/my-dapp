3f89db3e3a3a53467fdde41361b1f56e814c210b
Bolu75 <75iverson@gmail.com>
2026-01-15 03:02:51 -0800

    fix: final update to local imports for vercel

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,12 +1,11 @@
 import addresses from "./contract-addresses.json";
-
-// Correct path to reach the artifacts from the frontend folder
-import AMM_Artifact from "../../../backend/artifacts/contracts/CurveAMM.sol/CurveAMM.json";
-import Token_Artifact from "../../../backend/artifacts/contracts/MockToken.sol/MockToken.json";
+import AMM_Artifact from "./CurveAMM.json";
+import Token_Artifact from "./MockToken.json";
 
 export const AMM_ADDRESS = addresses.AMM;
 export const TOKEN_A_ADDRESS = addresses.TokenA;
 export const TOKEN_B_ADDRESS = addresses.TokenB;
 
+// Hardhat puts the ABI array inside an "abi" property of the JSON
 export const AMM_ABI = AMM_Artifact.abi;
 export const TOKEN_ABI = Token_Artifact.abi;
